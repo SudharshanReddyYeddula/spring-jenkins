88bfb239cd6ff5d0225346087e618ccead4f6b72
sudharshan <sudharshanreddy.mca@gmail.com>
2023-07-09 22:29:09 +0530

    wrong syntax for testing build in jenkins

--- src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
@@ -21,7 +21,7 @@ class SpringJenkinsApplicationTests {
 		logger.info("testing started...............");
 		logger.info("testing2 started...............");
 		logger.info("testing3 started...............");
-		assertEquals(true,true);
+		ssertEquals(true,true); // syntax - assertEquals(true,true)
 	}
 
 }
